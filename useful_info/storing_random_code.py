459e1d2466290befd05991a2455b1daf28cd27d8
TeunvdWeij <mailvanteun@gmail.com>
2023-10-10 10:37:55 +0200

    Reorganize code

--- useful_info/storing_random_code.py
+++
@@ -1,9 +0,0 @@
-# how to get gpt2-xl model architecture printed
-
-import torch
-from transformers import GPT2Tokenizer, GPT2Model
-name = "gpt2-xl"
-tokenizer = GPT2Tokenizer.from_pretrained(name)
-model = GPT2Model.from_pretrained(name, device_map="auto", torch_dtype=torch.half)
-
-print(model.modules)
