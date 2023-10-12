f0e28ef66b84df30b325d9c9e4d2252f76209422
TeunvdWeij <mailvanteun@gmail.com>
2023-10-12 11:26:20 +0200

    update error logging

--- bash_scripts/test.sh
+++ bash_scripts/test.sh
@@ -2,13 +2,15 @@
 #Set job requirements
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=rome
 #SBATCH --time=00:05:00
- 
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+
 #Loading modules
 module load 2022
-module load Python/3.10.4-GCCcore-11.3.0d
- 
-#Execute a Python program 
+module load Python/3.10.4-GCCcore-11.3.0
+
+#Execute a Python program
 python src/skip_tokens.py
