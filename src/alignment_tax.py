03bb0bc65578b6720ea4ef4548cdaae0a65db64f
TeunvdWeij <mailvanteun@gmail.com>
2023-10-13 17:02:49 +0200

    trying to make alignment_tax work with truncation

--- src/alignment_tax.py
+++ src/alignment_tax.py
@@ -4,23 +4,24 @@ import time
 import os
 
 from model import Llama2Helper
-from utils import load_pile, get_subset_from_dataset, get_hf_token, get_skip_tokens, acc
+from utils import load_pile, get_subset_from_dataset, get_hf_token, get_skip_tokens, acc, check_gpu_memory
 
 # first check if no file is being overwritten
-file_path = "results/alignment_tax_v0.1.json"
+file_path = "results/alignment_tax_v0.2.json"
 assert not os.path.isfile(file_path), "File already exists, nothing changed."
 
 
 model_name = "meta-llama/Llama-2-7b-hf"
 model = Llama2Helper(model_name=model_name, hf_token=get_hf_token())
 
-total_tokens = 1000
-batch_size = 2
+total_tokens = 100_000
+batch_size = 10
 mode = "all"
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"device is: {device}")
+# device = "cuda:1" if torch.cuda.is_available() else "cpu"
+# print(f"device is: {device}")
 
-skip_tokens = get_skip_tokens(mode=mode, skip="skip50", data_type="tokens_str")
+skip_tokens = get_skip_tokens(mode=mode, skip="skip50", data_type="tokens_int")
 skip_tokens = torch.tensor(skip_tokens).to(device)
 
 dataset = load_pile(split="train", mode=mode, batch_size=batch_size)
@@ -30,14 +31,15 @@ results = {}
 analyzed_tokens = 0
 batch_num = 0
 while analyzed_tokens < total_tokens:
-    print(f"Batch {batch_num}, {round(analyzed_tokens/total_tokens *100, 1)}% of total tokens")
+    print(f"Batch {batch_num}, {round(analyzed_tokens/total_tokens*100, 1)}% of total tokens")
+    check_gpu_memory()
     start_time = time.perf_counter()
     # could use this if OOM memory issues happen
     # torch.cuda.empty_cache()
     ds_subset = get_subset_from_dataset(dataset, batch_size)
 
-    # tokenize the text and pad to longest sequence. detach and to device for gpu memory usage
-    encoded = model.tokenizer(ds_subset, return_tensors="pt", padding="longest")["input_ids"].detach().to(device)
+    # truncate to context window, pad to longest sequence. detach and to device for gpu memory usage
+    encoded = model.tokenizer(ds_subset, truncation=True, max_length=4096, return_tensors="pt", padding="longest")["input_ids"].detach().to(device)
     
     # create filter (f) which checks if token is not padding
     #NOTE: this does mean that we never assess whether </s> is predicted correctly
@@ -70,9 +72,10 @@ while analyzed_tokens < total_tokens:
         }
     
     batch_num += 1
+    analyzed_tokens += torch.sum(f).item()
 
     # temporary safety measure
-    if batch_num > 5:
+    if batch_num > 100:
         break
 
 with open(file_path, "w") as f:
