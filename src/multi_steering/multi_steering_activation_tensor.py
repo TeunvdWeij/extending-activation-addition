07ecab3935afebf09a266be7e2ce41579e2c0bf1
TeunvdWeij <mailvanteun@gmail.com>
2023-12-23 21:58:36 +0100

    first multi steering functionality, still contains bugs

---
+++ src/multi_steering/multi_steering_activation_tensor.py
@@ -0,0 +1,64 @@
+import os
+import torch
+
+
+class MultiSteeringActivationTensor:
+    """Class to save activations and its meta data"""
+
+    def __init__(
+        self,
+        note,
+        version,
+        name,
+        n_layers,
+        device,
+    ):
+        self.note = note
+        self.version = version
+        self.name = name
+
+        # create the placeholder for the activations based on the number of 
+        # layers in model and the model's hidden dimension
+        self.acts = torch.zeros((n_layers, 4096)).to(device)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def check_acts(self, acts):
+        """Check whether values in acts are correct."""
+        if torch.isinf(acts).any().item():
+            raise ValueError("Infinite value in acts.")
+
+        if torch.isnan(acts).any().item():
+            raise ValueError("Nan value in acts.")
+
+
+    def process_new_acts(self, new_acts, i, layer_idx):
+        """
+        new_acts: activations of model of certain layer
+        i: sample index
+        layer_idx: layer index
+        """
+        self.check_acts(new_acts)
+        self.acts[layer_idx] = (self.acts[layer_idx] * i + new_acts) / (i + 1)
+
+    def generate_save_path_string(self):
+        folder = "data/activations/multi_steering/"
+
+        if not os.path.isdir(folder):
+            raise FileNotFoundError("Path does not exist.")
+        save_path = folder + f"{self.name}.pt"
+
+        if os.path.isfile(save_path):
+            raise FileExistsError("File already exists, can't overwrite file.")
+
+        print(f"SAVE PATH: {save_path}")
+        self.save_path = save_path
+        
+    def save(self):
+        self.generate_save_path_string()
+        try:
+            torch.save(self, self.save_path)
+            print(f"SUCCESS: Tensor saved with metadata at {self.save_path}")
+        except Exception as e:
+            print(f"FAILED: Could not save the tensor. Error: {e}")
---
+++ src/multi_steering/multi_steering_generate_activations.py
@@ -0,0 +1,85 @@
+import argparse
+import tqdm
+
+from model import Llama2Helper
+from utils import get_hf_token, encode, load_multi_steering_data, get_model_name
+from multi_steering_activation_tensor import MultiSteeringActivationTensor
+
+DATASET_NAMES = [
+        "myopic",
+        "wealth_seeking", 
+        "sycophancy",
+        "agreeableness",
+        "anti_immigration",
+    ]
+
+def get_activations(note, version, params, chat):
+    model_name = get_model_name(params, chat)
+    model = Llama2Helper(
+        model_name=model_name, hf_token=get_hf_token(),
+    )
+    datasets = load_multi_steering_data()
+    
+    activations_dict = {}
+    for name, data in datasets.items():
+        acts = MultiSteeringActivationTensor(note, version, name, model.n_layers, model.device)
+        # :500 to take as training data for the steering vectors, note that they are randomized before
+        for sample_idx, row in tqdm(enumerate(data[:500]), desc=f'Processing {name} prompts'):
+            q = row["question"]
+            match_q = q + row['answer_matching_behavior']
+            encoded = encode(model, match_q)
+        
+            # get the activations associated to dataset behaviour
+            model.get_logits(encoded)
+            matching_acts = []
+            for layer_idx in range(model.n_layers):
+                activations = model.get_last_activations(layer_idx)
+                activations = activations[0, -1, :].detach()
+                matching_acts.append(activations)
+                
+            # get the activations associated to non-sycopahntic behaviour
+            non_match_q = q + row['answer_not_matching_behavior']
+            encoded = encode(model, non_match_q)
+            
+            model.get_logits(encoded)
+            non_matching_acts = []
+            for layer_idx in range(model.devicen_layers):
+                activations = model.get_last_activations(layer_idx)
+                activations = activations[0, -1, :].detach()
+                non_matching_acts.append(activations)
+        
+            # get the difference in activations and calculate mean
+            for layer_idx in range(model.n_layers):
+                new_acts = (matching_acts[layer_idx] - non_matching_acts[layer_idx]).to(model.device)
+                acts.process_new_acts(new_acts, sample_idx, layer_idx)
+
+        activations_dict[name] = acts
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+
+    # required params
+    parser.add_argument("--note", type=str)
+    parser.add_argument("--version", type=str)
+
+    # optional params
+    parser.add_argument("--chat", default=True, action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        "--model_params", type=str, choices=["7b", "13b", "70b"], default="7b"
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = arg_parser()
+    get_activations(
+        args.note, 
+        args.version,
+        args.model_params,
+        args.chat,
+        )
+
+if __name__ == "__main__":
+    main()
