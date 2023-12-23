092043df519645c24c70a5374f1a7f618fc4ae38
TeunvdWeij <mailvanteun@gmail.com>
2023-12-23 21:56:52 +0100

    ruff format

--- src/activation_tensor.py
+++ src/activation_tensor.py
@@ -17,7 +17,7 @@ class ActivationTensor:
         chat,
         layer_idx,
         max_seq_length,
-        truncation, #truncation is unused
+        truncation,  # truncation is unused
         mean,
         dtype,
     ):
